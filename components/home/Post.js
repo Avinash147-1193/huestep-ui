278dce7426f175eb721d0fcf140a2bf2b2bb6784
Avinash Dwivedi <avinash@Avinashs-MacBook-Pro.local>
2023-06-29 19:27:11 +0530

    post can load video now

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,11 +1,13 @@
-import  React, {useState, useEffect} from 'react'
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, Dimensions } from 'react-native';
+import Constants from 'expo-constants';
+
 import { Video } from 'expo-av';
 import * as ImagePicker from 'expo-image-picker';
-import { Divider } from 'react-native-elements'
-import { GlobalColors } from '../../constants/GlobalColors'
-import { API } from '../../constants/GlobalAPI'
-import { POSTS } from '../../data/Posts'
+import { Divider } from 'react-native-elements';
+import { GlobalColors } from '../../constants/GlobalColors';
+import { API } from '../../constants/GlobalAPI';
+import { POSTS } from '../../data/Posts';
 import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
 import { setAuthUserProfile, setAuthUserPost, setAuthUserLikedPost } from '../../redux/actions';
@@ -32,66 +34,78 @@ const postFooterIcons = [
   }
 ]
 
-
-
-const Post = ({post}) => {
+const Post = ({ post }) => {
   const [like, setLike] = useState('');
   const [likesCount, setLikesCount] = useState('');
   const { user_liked_posts, user } = useSelector(state => state.userReducer);
-  //const dispatch = useDispatch();
+  const scrollViewRef = useRef(null);
+  const [videoInView, setVideoInView] = useState(false);
 
 
   useEffect(() => {
     setLikesCount(post.fields.likes)
     if (user_liked_posts.indexOf(post.pk) >= 0) {
       setLike(1);
-      console.log('----usereffect if----', post.fields.likes,'--------likes---',post.fields.likes,'---------post.pk----',post.pk,'------user_liked_posts----', user_liked_posts)
+      console.log('----usereffect if----', post.fields.likes, '--------likes---', post.fields.likes, '---------post.pk----', post.pk, '------user_liked_posts----', user_liked_posts)
     } else {
       setLike(0);
     }
-
   }, [user_liked_posts, post.pk]);
 
-  const HandleLike =  (post, user_liked_posts) => {
+  const HandleLike = (post, user_liked_posts) => {
     let updatedLikedPosts = []
 
-    like == 0 ? setLikesCount(likesCount + 1 ) : setLikesCount(likesCount - 1 )
+    like == 0 ? setLikesCount(likesCount + 1) : setLikesCount(likesCount - 1)
     setLike(!like);
 
     try {
       let baseUrl = API[SERVER_STATE] + API.USER.postLike;
-       console.log(baseUrl, '---username1--', user.user_id, '---token----',user.token);
+      console.log(baseUrl, '---username1--', user.user_id, '---token----', user.token);
       const response = axios.post(baseUrl, {
-          username: user.user_id,
-          token: user.token,
-          post: {post_id: post.pk}
+        username: user.user_id,
+        token: user.token,
+        post: { post_id: post.pk }
       });
 
     } catch (error) {
       console.log(error);
     }
   }
+
+  const handleScroll = (event) => {
+    const scrollPosition = event.nativeEvent.contentOffset.y;
+    const screenHeight = Dimensions.get('window').height - Constants.statusBarHeight;
+    const isVideoInView = scrollPosition > 0 && scrollPosition < screenHeight;
+    
+    setVideoInView(isVideoInView);
+  };
   
+  
+  
+
   return (
-    <View style={{marginBottom: 30}}>
+    <View style={{ marginBottom: 30 }}>
       <PostHeader post={post} />
-      <PostImage post={post} />
-      <View style={{marginHorizontal: 15, marginTop: 10}}>
-        <PostFooter post={post} user_liked_posts={user_liked_posts} HandleLike={HandleLike} like={like}/>
-        <Likes post={post} likesCount={likesCount}/>
-        <Caption post={post} />
-        <CommentSection post={post} />
-        <Comments post={post} />
-        <Divider width={1} orientation="vertical" style={styles.divider}/>
-      </View>
-      
+      <ScrollView
+        ref={scrollViewRef}
+        onScroll={handleScroll}
+        scrollEventThrottle={16}
+      >
+        <PostImage post={post} scrollViewRef={scrollViewRef} videoInView={videoInView} setVideoInView={setVideoInView} />
+        <View style={{ marginHorizontal: 15, marginTop: 10 }}>
+          <PostFooter post={post} user_liked_posts={user_liked_posts} HandleLike={HandleLike} like={like} />
+          <Likes post={post} likesCount={likesCount} />
+          <Caption post={post} />
+          <CommentSection post={post} />
+          <Comments post={post} />
+          <Divider width={1} orientation="vertical" style={styles.divider} />
+        </View>
+      </ScrollView>
     </View>
   )
 }
 
-
-
-const PostHeader = ({post}) => (
+const PostHeader = ({ post }) => (
   <View
     style={{
       flexDirection: 'row',
@@ -100,19 +114,25 @@ const PostHeader = ({post}) => (
       alignItems: 'center',
     }}
   >
-    <View style={{flexDirection: 'row', alignItems: 'center'}}>
-      <Image source={{uri: post.fields.profile_picture}} style={styles.story}/>
-      <Text style={{color: GlobalColors.text.postText, marginLeft:5, fontWeight: '700'}}>{post.fields.username}</Text>
+    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <Image source={{ uri: post.fields.profile_picture }} style={styles.story} />
+      <Text style={{ color: GlobalColors.text.postText, marginLeft: 5, fontWeight: '700' }}>{post.fields.username}</Text>
     </View>
     <TouchableOpacity>
-      <Text style={{color:  GlobalColors.text.postText, fontWeight: '900'}}>...</Text>
+      <Text style={{ color: GlobalColors.text.postText, fontWeight: '900' }}>...</Text>
     </TouchableOpacity>
   </View>
 )
 
-const PostImage = ({ post }) => {
+const PostImage = ({ post, scrollViewRef, videoInView, setVideoInView }) => {
   const postImages = post.fields.post_images;
 
+  const handlePlaybackStatusUpdate = (status) => {
+    const { isPlaying, positionMillis, durationMillis } = status;
+    const isVideoInView = isPlaying && positionMillis < durationMillis / 2;
+    setVideoInView(isVideoInView);
+  };
+  
   if (postImages.endsWith('.jpg') || postImages.endsWith('.png') || postImages.endsWith('.gif')) {
     return (
       <View style={{
@@ -129,11 +149,13 @@ const PostImage = ({ post }) => {
         height: 450,
       }}>
         <Video
-          source={{ uri: postImages }}
-          style={{ height: '100%', resizeMode: 'cover' }}
-          shouldPlay
-          useNativeControls
-        />
+  source={{ uri: postImages }}
+  style={{ height: '100%', resizeMode: 'cover' }}
+  shouldPlay={videoInView}
+  useNativeControls
+  onPlaybackStatusUpdate={handlePlaybackStatusUpdate}
+/>
+
       </View>
     );
   } else {
@@ -143,46 +165,46 @@ const PostImage = ({ post }) => {
 
 
 
-const PostFooter = ( {post, user_liked_posts, HandleLike, like} ) => (
+const PostFooter = ({ post, user_liked_posts, HandleLike, like }) => (
 
-  <View style={{flexDirection: 'row',}}>
+  <View style={{ flexDirection: 'row', }}>
     <View style={styles.leftFooterIconsContainer}>
-      <Icon imageStyle={styles.footerIcon} onPress={() => HandleLike(post, user_liked_posts)} imgUrl={ like == 1 ? postFooterIcons[0].likedImageUrl : postFooterIcons[0].imageUrl} />
+      <Icon imageStyle={styles.footerIcon} onPress={() => HandleLike(post, user_liked_posts)} imgUrl={like == 1 ? postFooterIcons[0].likedImageUrl : postFooterIcons[0].imageUrl} />
       <Icon imageStyle={styles.footerIcon} onPress={() => console.log('-------comment-------')} imgUrl={postFooterIcons[1].imageUrl} />
       <Icon imageStyle={styles.footerIcon} onPress={() => console.log('-------share-------')} imgUrl={postFooterIcons[2].imageUrl} />
     </View>
-    <View style={{flex:1, alignItems:'flex-end'}}>
-      <Icon imageStyle={styles.footerIcon} imgUrl={postFooterIcons[3].imageUrl} />
+    <View style={{ flex: 1, alignItems: 'flex-end' }}>
+      <Icon imageStyle={styles.footerIcon} onPress={() => console.log('-------save-------')} imgUrl={postFooterIcons[3].imageUrl} />
     </View>
   </View>
-  
+
 )
 
-const Icon = ({imageStyle, imgUrl, onPress}) => (
+const Icon = ({ imageStyle, imgUrl, onPress }) => (
   <TouchableOpacity onPress={onPress}>
-    <Image style={imageStyle} source={{uri: imgUrl}} />
+    <Image style={imageStyle} source={{ uri: imgUrl }} />
   </TouchableOpacity>
 )
 
-const Likes = ({post, likesCount}) => (
-  <View style={{flexDirection: 'row', marginTop: 4}}>
-    <Text style={{color:  GlobalColors.text.postText, fontWeight: '600'}} >{likesCount} likes</Text>
+const Likes = ({ post, likesCount }) => (
+  <View style={{ flexDirection: 'row', marginTop: 4 }}>
+    <Text style={{ color: GlobalColors.text.postText, fontWeight: '600' }} >{likesCount} likes</Text>
   </View>
 )
 
-const Caption = ({post}) => (
-  <View style={{marginTop:5}}>
-    <Text style={{color:  GlobalColors.text.postText}}>
-      <Text style={{fontWeight: 600}}>{POSTS[0].user}</Text>
+const Caption = ({ post }) => (
+  <View style={{ marginTop: 5 }}>
+    <Text style={{ color: GlobalColors.text.postText }}>
+      <Text style={{ fontWeight: 600 }}>{POSTS[0].user}</Text>
       <Text> {post.fields.text}</Text>
     </Text>
   </View>
 )
 
-const CommentSection = ({post}) => (
-  <View style={{marginTop: 5}}>
+const CommentSection = ({ post }) => (
+  <View style={{ marginTop: 5 }}>
     {!!post.fields.comments.length && (
-      <Text style={{color:GlobalColors.text.postText}}>
+      <Text style={{ color: GlobalColors.text.postText }}>
         View{post.fields.comments.length > 1 ? ' all' : ''} {post.fields.comments.length}
         {post.fields.comments.length > 1 ? ' comments' : ' comment'}
       </Text>
@@ -190,10 +212,10 @@ const CommentSection = ({post}) => (
   </View>
 )
 
-const Comments = ({post}) => (
-  <View style={{flexDirection: 'row', marginTop: 5}}>
-    <Text style={{color: GlobalColors.text.postText}}>
-      <Text style={{fontWeight: '600'}}>{POSTS[0].comments[0].user}</Text>
+const Comments = ({ post }) => (
+  <View style={{ flexDirection: 'row', marginTop: 5 }}>
+    <Text style={{ color: GlobalColors.text.postText }}>
+      <Text style={{ fontWeight: '600' }}>{POSTS[0].comments[0].user}</Text>
       <Text>{POSTS[0].comments[0].comment}</Text>
     </Text>
   </View>
@@ -203,12 +225,12 @@ export default Post
 
 const styles = StyleSheet.create({
   story: {
-      width: 35,
-      height: 35,
-      borderRadius: 50,
-      marginLeft: 10,
-      borderWidth: 1.5,
-      borderColor: GlobalColors.elements.storyBorderColor
+    width: 35,
+    height: 35,
+    borderRadius: 50,
+    marginLeft: 10,
+    borderWidth: 1.5,
+    borderColor: GlobalColors.elements.storyBorderColor
   },
   footerIcon: {
     width: 25,
@@ -222,5 +244,9 @@ const styles = StyleSheet.create({
   },
   divider: {
     marginTop: 10,
-  }
-})
+  },
+  scrollView: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,106 +1,5 @@
-// import React, { useEffect, useState } from 'react';
-// import { View, Text, SafeAreaView, StyleSheet, ScrollView } from 'react-native';
-// import Header from '../components/home/Header';
-// import { GlobalColors } from '../constants/GlobalColors';
-// import Stories from '../components/home/Stories';
-// import Post from '../components/home/Post';
-// import BottomTabs, { bottomTabIcon } from '../components/home/BottomTabs';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { setAuthUserProfile, setAuthUserPost } from '../redux/actions';
-// import { API } from '../constants/GlobalAPI';
-// import axios from 'axios';
-
-// const SERVER_STATE = API.CURRENT_STATE;
-
-// const HomeScreen = ({ navigation }) => {
-//   const { user_profile } = useSelector(state => state.userReducer);
-//   const { user_post } = useSelector(state => state.userReducer);
-//   const { user } = useSelector(state => state.userReducer);
-//   const authToken = user.token;
-//   const username = user.user_id;
-//   const dispatch = useDispatch();
-//   console.log('-------user-----', username, authToken)
-
-
-//   useEffect(() => {
-//     const fetchUserProfile = async () => {
-//       try {
-//         let baseUrl = API[SERVER_STATE] + API.USER.profile;
-//        // console.log(baseUrl, '---username1--', username, '---token----',authToken);
-//         const response = await axios.post(baseUrl, {
-//           username: username,
-//           token: authToken,
-//         });
-//         dispatch(setAuthUserProfile(response.data))
-//         //console.log(response.data)
-//       } catch (error) {
-//         //console.log(error);
-//       }
-//     };
-
-//     const fetchUserPosts = async () => {
-//       try {
-//         let baseUrl = API[SERVER_STATE] + API.USER.post;
-//         //console.log(baseUrl, '---username2--', username, '---token----', authToken);
-//         const response2 = await axios.get(baseUrl, {
-//           params: {
-//             username: username,
-//             token: authToken,
-//             post: {}
-//           },
-//         });
-//         dispatch(setAuthUserPost(response2.data.post.data));
-//         console.log(response2.data.post.data);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-
-//     fetchUserProfile();
-//     fetchUserPosts();
-//   }, []);
-
-//   if (!user_post || user_post.length === 0) {
-//     return null; // Return null or a placeholder component
-    
-//   }
-
-//   //console.log('---------------user profile ----user post---------',user_post, user_post[0].fields.post_images)
-//   const posts = Object.values(user_post).map(item => item.fields);
-
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <Header navigation={navigation} />
-//       <Stories/>
-//       <ScrollView style={{ marginTop: 5}}>
-//         {posts.map((post, index) => (
-//           <View key={index}>
-//             <Post post={post} />
-//           </View>
-//         ))}
-//       </ScrollView>
-//       <BottomTabs icons={bottomTabIcon} />
-//     </SafeAreaView>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: GlobalColors.primary.black,
-//     flex: 1,
-//   },
-// });
-
-// export default HomeScreen;
-
-
-
-
-
-
-
 import React, { useEffect, useState, useRef } from 'react';
-import { View, Text, SafeAreaView, StyleSheet, ScrollView, Animated, Platform  } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, ScrollView, Animated, Platform, Dimensions  } from 'react-native';
 import Header from '../components/home/Header';
 import { GlobalColors } from '../constants/GlobalColors';
 import Stories from '../components/home/Stories';
@@ -122,6 +21,8 @@ const HomeScreen = ({ navigation }) => {
   const { user_post } = useSelector(state => state.userReducer);
   const { user_liked_posts } = useSelector(state => state.userReducer);
   const { user } = useSelector(state => state.userReducer);
+  const [videoInView, setVideoInView] = useState(false);
+
   const authToken = user.token;
   const username = user.user_id;
   const dispatch = useDispatch();
@@ -187,17 +88,16 @@ const HomeScreen = ({ navigation }) => {
   const posts = Object.values(user_post).map(item => item);
 
   const handleScroll = (event) => {
-    const offsetY = event.nativeEvent.contentOffset.y;
-
-    const opacity = Math.max(1 - offsetY / HEADER_HEIGHT, 0);
-    const translateY = -offsetY;
-
-    headerOpacity.setValue(opacity);
-    scrollY.setValue(offsetY);
-    headerContainerRef.current.setNativeProps({
-      style: { transform: [{ translateY }] },
-    });
+    const scrollPosition = event.nativeEvent.contentOffset.y;
+    const screenHeight = Dimensions.get('window').height;
+  
+    if (scrollPosition > 0 && scrollPosition < screenHeight) {
+      setVideoInView(true);
+    } else {
+      setVideoInView(false);
+    }
   };
+  
 
   if (!user_post || user_post.length === 0) {
     return (
@@ -241,7 +141,7 @@ const HomeScreen = ({ navigation }) => {
         >
           {posts.map((post, index) => (
             <View key={index}>
-              <Post post={post} />
+              <Post post={post} videoInView={videoInView} />
             </View>
           ))}
         </ScrollView>
